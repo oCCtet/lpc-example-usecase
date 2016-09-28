f44f9fb3b6406c0fb8e23499e09ff0181e39908e
Sami Hartikainen <sami.hartikainen@teleste.com>
2016-09-28 17:43:48 +0300

    gosu: Update to release 1.9

--- build.sh
+++ build.sh
@@ -5,12 +5,13 @@
 
 set -e
 
-GOSU_RELEASE="1.7"
-TARGET_ARCH="$(dpkg --print-architecture)"
+GOSU_RELEASE="1.9"
+TARGET_ARCH="$(dpkg --print-architecture | awk -F- '{ print $NF }')"
 
 rm -f debian/gosu centos/gosu
 curl -o debian/gosu -sSL "https://github.com/tianon/gosu/releases/download/$GOSU_RELEASE/gosu-$TARGET_ARCH"
+chmod +x debian/gosu
 cp debian/gosu centos/
 
-docker build -t occtet/lpc-streaming-usecase debian
-docker build -t occtet/lpc-streaming-usecase-min centos
+docker build --pull -t occtet/lpc-streaming-usecase debian
+docker build --pull -t occtet/lpc-streaming-usecase-min centos
